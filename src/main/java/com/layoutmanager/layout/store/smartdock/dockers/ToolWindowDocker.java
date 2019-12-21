0bbb2bb17e073bcce56effb443827a2bb558cb35
Michael Estermann <michael.estermann@erowa.com>
2019-12-21 23:24:30 +0100

    Tool window docking extended.

--- src/main/java/com/layoutmanager/layout/store/smartdock/dockers/ToolWindowDocker.java
+++ src/main/java/com/layoutmanager/layout/store/smartdock/dockers/ToolWindowDocker.java
@@ -5,64 +5,99 @@ import com.layoutmanager.layout.store.smartdock.ToolWindowDocking;
 import java.awt.Rectangle;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 
 public class ToolWindowDocker {
     public void dock(ToolWindowDocking[] floatedOrWindowsToolWindows) {
         this.dockLeft(floatedOrWindowsToolWindows);
         this.dockTop(floatedOrWindowsToolWindows);
-        // TODO: Add bottom and left in a more generic way...
-        // Idea: Introduce class DockingBounds and use this in the ToolWindowDocking
+        this.dockRight(floatedOrWindowsToolWindows);
+        this.dockBottom(floatedOrWindowsToolWindows);
     }
 
     private void dockLeft(ToolWindowDocking[] floatedOrWindowsToolWindows) {
+        this.dockToolWindow(
+            floatedOrWindowsToolWindows,
+            (x, y) -> (int) (x.getBounds().getX() - y.getBounds().getX()),
+            (x, y) -> x.getRightDockingBounds().intersects(y.getLeftDockingBounds()),
+            Comparator.comparingInt(x -> (int) x.getBounds().getY()),
+            (toolWindow, boundsToDock) -> {
+                int x = (int) toolWindow.getBounds().getX();
+                int newX = (int) boundsToDock.getMaxX();
+                return new Rectangle(
+                    newX,
+                    (int) toolWindow.getBounds().getY(),
+                    (int) (toolWindow.getBounds().getWidth() + (x - newX)),
+                    (int) toolWindow.getBounds().getHeight());
+            });
+    }
+
+    private void dockTop(ToolWindowDocking[] floatedOrWindowsToolWindows) {
+        this.dockToolWindow(
+            floatedOrWindowsToolWindows,
+            (x, y) -> (int) (x.getBounds().getY() - y.getBounds().getY()),
+            (x, y) -> x.getBottomDockingBounds().intersects(y.getTopDockingBounds()),
+            Comparator.comparingInt(x -> (int) x.getBounds().getX()),
+            (toolWindow, boundsToDock) -> {
+                int y = (int) toolWindow.getBounds().getY();
+                int newY = (int) boundsToDock.getMaxY();
+                return new Rectangle(
+                    (int) toolWindow.getBounds().getX(),
+                    newY,
+                    (int) toolWindow.getBounds().getWidth(),
+                    (int) (toolWindow.getBounds().getHeight() + (y - newY)));
+            });
+    }
+
+    private void dockRight(ToolWindowDocking[] floatedOrWindowsToolWindows) {
+        this.dockToolWindow(
+            floatedOrWindowsToolWindows,
+            (x, y) -> (int) (x.getBounds().getMaxX() - y.getBounds().getMaxX()),
+            (x, y) -> x.getLeftDockingBounds().intersects(y.getRightDockingBounds()),
+            Comparator.comparingInt(x -> (int) x.getBounds().getY()),
+            (toolWindow, boundsToDock) -> new Rectangle(
+                (int) toolWindow.getBounds().getX(),
+                (int) toolWindow.getBounds().getY(),
+                (int) (boundsToDock.getX() - toolWindow.getBounds().getX()),
+                (int) toolWindow.getBounds().getHeight()));
+    }
+
+    private void dockBottom(ToolWindowDocking[] floatedOrWindowsToolWindows) {
+        this.dockToolWindow(
+            floatedOrWindowsToolWindows,
+            (x, y) -> (int) (x.getBounds().getMaxY() - y.getBounds().getMaxY()),
+            (x, y) -> x.getTopDockingBounds().intersects(y.getBottomDockingBounds()),
+            Comparator.comparingInt(x -> (int) x.getBounds().getY()),
+            (toolWindow, boundsToDock) -> new Rectangle(
+                (int) toolWindow.getBounds().getX(),
+                (int) toolWindow.getBounds().getY(),
+                (int) toolWindow.getBounds().getWidth(),
+                (int) (boundsToDock.getY() - toolWindow.getBounds().getY())));
+    }
+
+    private void dockToolWindow(
+            ToolWindowDocking[] floatedOrWindowsToolWindows,
+            Comparator<? super ToolWindowDocking> sortForProcessing,
+            BiPredicate<ToolWindowDocking, ToolWindowDocking> isIntersecting,
+            Comparator<? super ToolWindowDocking> sortForClosestToDock,
+            BiFunction<ToolWindowDocking, Rectangle, Rectangle> calculateNewBounds) {
+
         ToolWindowDocking[] sortedByLeftPosition = Arrays.stream(floatedOrWindowsToolWindows)
-                .sorted((x, y) -> (int) (x.getBounds().getX() - y.getBounds().getX()))
+                .sorted(sortForProcessing)
                 .skip(1)
                 .toArray(ToolWindowDocking[]::new);
 
         for (ToolWindowDocking toolWindowDocking : sortedByLeftPosition) {
-            ToolWindowDocking[] intersectionToolWindows = Arrays.stream(floatedOrWindowsToolWindows)
-                    .filter(x ->
-                            !x.getToolWindowInfo().getId().equals(toolWindowDocking.getToolWindowInfo().getId()) &&
-                            x.getRightDockingBounds().intersects(toolWindowDocking.getLeftDockingBounds()))
-                    .sorted(Comparator.comparingInt(x -> (int) x.getBounds().getY()))
-                    .toArray(ToolWindowDocking[]::new);
+            ToolWindowDocking dockingTarget = Arrays.stream(floatedOrWindowsToolWindows)
+                .filter(x ->
+                    !x.getToolWindowInfo().getId().equals(toolWindowDocking.getToolWindowInfo().getId()) &&
+                    isIntersecting.test(x, toolWindowDocking))
+                .min(sortForClosestToDock)
+                .orElse(null);
 
-            if (intersectionToolWindows.length > 0) {
-                int x = (int)toolWindowDocking.getBounds().getX();
-                int newX = (int)intersectionToolWindows[0].getBounds().getMaxX();
-                Rectangle newBounds = new Rectangle(
-                        newX,
-                        (int) toolWindowDocking.getBounds().getY(),
-                        (int)(toolWindowDocking.getBounds().getWidth() + (x - newX)),
-                        (int) toolWindowDocking.getBounds().getHeight());
-                toolWindowDocking.getToolWindowInfo().setBounds(newBounds);
-            }
-        }
-    }
-
-    private void dockTop(ToolWindowDocking[] floatedOrWindowsToolWindows) {
-        ToolWindowDocking[] sortedByTopPosition = Arrays.stream(floatedOrWindowsToolWindows)
-                .sorted((x, y) -> (int) (x.getBounds().getY() - y.getBounds().getY()))
-                .skip(1)
-                .toArray(ToolWindowDocking[]::new);
-
-        for (ToolWindowDocking toolWindowDocking : sortedByTopPosition) {
-            ToolWindowDocking[] intersectionToolWindows = Arrays.stream(floatedOrWindowsToolWindows)
-                    .filter(x ->
-                            !x.getToolWindowInfo().getId().equals(toolWindowDocking.getToolWindowInfo().getId()) &&
-                            x.getBottomDockingBounds().intersects(toolWindowDocking.getTopDockingBounds()))
-                    .sorted(Comparator.comparingInt(x -> (int) x.getBounds().getX()))
-                    .toArray(ToolWindowDocking[]::new);
-
-            if (intersectionToolWindows.length > 0) {
-                int y = (int)toolWindowDocking.getBounds().getY();
-                int newY = (int)intersectionToolWindows[0].getBounds().getMaxY();
-                Rectangle newBounds = new Rectangle(
-                        (int) toolWindowDocking.getBounds().getX(),
-                        newY,
-                        (int) toolWindowDocking.getBounds().getWidth(),
-                        (int)(toolWindowDocking.getBounds().getHeight() + (y - newY)));
+            if (dockingTarget != null) {
+                Rectangle newBounds = calculateNewBounds.apply(toolWindowDocking, dockingTarget.getBounds());
                 toolWindowDocking.getToolWindowInfo().setBounds(newBounds);
             }
         }

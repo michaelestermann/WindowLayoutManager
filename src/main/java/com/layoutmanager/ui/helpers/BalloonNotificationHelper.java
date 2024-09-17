6672abda0a13313bc47f55d0c405499ee4892052
Michael Estermann <14977799+michaelestermann@users.noreply.github.com>
2024-09-17 13:51:48 +0200

    Fix deprecated notification api

--- src/main/java/com/layoutmanager/ui/helpers/BalloonNotificationHelper.java
+++ src/main/java/com/layoutmanager/ui/helpers/BalloonNotificationHelper.java
@@ -1,12 +1,11 @@
 package com.layoutmanager.ui.helpers;
 
 import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationDisplayType;
-import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationGroupManager;
 import com.intellij.notification.NotificationType;
-import com.intellij.notification.Notifications;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
+
 import java.util.Arrays;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -14,31 +13,24 @@ import java.util.concurrent.TimeUnit;
 
 public class BalloonNotificationHelper {
     private static final int NOTIFICATION_DISPLAY_TIME_IN_SECONDS = 6;
-    private static final NotificationGroup LAYOUT_MANAGER_GROUP =
-            new NotificationGroup("WindowLayoutManager", NotificationDisplayType.BALLOON, true);
 
     public static void info(String title, String content) {
-        Notification notification = LAYOUT_MANAGER_GROUP.createNotification(
-                title,
-                null,
-                content,
-                NotificationType.INFORMATION);
+        Notification notification = NotificationGroupManager.getInstance()
+                .getNotificationGroup("WindowLayoutManager")
+                .createNotification(title, content, NotificationType.INFORMATION);
         notify(notification);
     }
 
     public static void warning(String title, String content) {
-        Notification notification = LAYOUT_MANAGER_GROUP.createNotification(
-                title,
-                null,
-                content,
-                NotificationType.WARNING);
+        Notification notification = NotificationGroupManager.getInstance()
+                .getNotificationGroup("WindowLayoutManager")
+                .createNotification(title, content, NotificationType.WARNING);
         notify(notification);
     }
 
     private static void notify(Notification notification) {
         Project currentProject = getCurrentProject();
-        Notifications.Bus.notify(notification, currentProject);
-
+        notification.notify(currentProject);
         hideAfterTimeSpan(notification);
     }
 
@@ -51,9 +43,7 @@ public class BalloonNotificationHelper {
     }
 
     private static Project getCurrentProject() {
-        Project[] openedProjects = ProjectManager
-                .getInstance()
-                .getOpenProjects();
+        Project[] openedProjects = ProjectManager.getInstance().getOpenProjects();
         return Arrays.stream(openedProjects)
                 .findFirst()
                 .orElse(null);
